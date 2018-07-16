2ec481d14872e581ac49226e8d67baa3b2c1e67a
Quan <hokagequan@me.com>
2018-07-17 00:07:02 +0800

    autoclick

--- core/drive.py
+++ core/drive.py
@@ -5,3 +5,17 @@ mouse = Controller()
 
 print('The current pointer position is {0}'.format(mouse.position))
 
+def position_mouse(x, y):
+	mouse.position = (x, y)
+
+def click():
+	mouse.click(Button.left, 1)
+
+def press_mouse():
+	mouse.press(Button.left)
+
+def release_mouse():
+	mouse.release(Button.left)
+
+def move_mouse(x, y):
+	mouse.move(x, y)
--- tao/tao.py
+++ tao/tao.py
@@ -2,4 +2,11 @@ import sys
 sys.path.append("..")
 
 from core import drive
+from core import mouse
 
+def start():
+	pass
+
+
+if __name__ == '__main__':
+	pass
