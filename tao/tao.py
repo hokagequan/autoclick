98509783c92d5a67d5abc9446b16524ea541c856
Quan <hokagequan@me.com>
2018-07-17 00:46:26 +0800

    线程

--- tao/tao.py
+++ tao/tao.py
@@ -1,12 +1,18 @@
 import sys
+import threading
 sys.path.append("..")
 
 from core import drive
 from core import mouse
 
+def work():
+	
+
 def start():
-	pass
+	t = threading.Thread(target=work)
+	threading.append(t)
+	t.start()
 
 
 if __name__ == '__main__':
-	pass
+	start()
