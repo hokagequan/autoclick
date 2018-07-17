dcc217d6772f2e538d05b526e845a54eebff1470
QiM <hokagequan@me.com>
2018-07-17 11:26:36 +0800

    鼠标控制

--- core/drive.py
+++ core/drive.py
@@ -1,21 +1,48 @@
 
 from pynput.mouse import Button, Controller
+from pynput import keyboard
+
+
+esc_pressed = False
+
+
+# def on_press(key):
+#     if key == keyboard.Key.esc:
+#         esc_pressed = True
+
+
+# def on_release(key):
+#     pass
+
+
+# with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+#     listener.join()
+
 
 mouse = Controller()
 
 print('The current pointer position is {0}'.format(mouse.position))
 
+
 def position_mouse(x, y):
-	mouse.position = (x, y)
+    mouse.position = (x, y)
+
 
 def click():
-	mouse.click(Button.left, 1)
+    mouse.click(Button.left, 1)
+
 
 def press_mouse():
-	mouse.press(Button.left)
+    mouse.press(Button.left)
+
 
 def release_mouse():
-	mouse.release(Button.left)
+    mouse.release(Button.left)
+
 
 def move_mouse(x, y):
-	mouse.move(x, y)
+    mouse.move(x, y)
+
+
+def scroll_mouse(x, step):
+    mouse.scroll(x, step)
--- tao/tao.py
+++ tao/tao.py
@@ -1,18 +1,45 @@
 import sys
 import threading
+import time
 sys.path.append("..")
 
 from core import drive
-from core import mouse
+# from core import mouse
+
+
+click_point = (150, 200)
+back_point = (30, 20)
+move_start_point = (150, 500)
+move_distance = (0, 80)
+
 
 def work():
-	
+    while not drive.esc_pressed:
+        time.sleep(2)
+
+        # drive.position_mouse(*click_point)
+        # drive.click()
+
+        # time.sleep(20)
+
+        # drive.position_mouse(*back_point)
+        # drive.click()
+
+        print("move mouse")
+        # drive.position_mouse(*move_start_point)
+        # drive.press_mouse()
+        # drive.move_mouse(*move_distance)
+        # drive.release_mouse()
+        drive.position_mouse(*click_point)
+        drive.scroll_mouse(0, -200)
+        print("move mouse end")
+
 
 def start():
-	t = threading.Thread(target=work)
-	threading.append(t)
-	t.start()
+    t = threading.Thread(target=work)
+    t.start()
+    t.join()
 
 
 if __name__ == '__main__':
-	start()
+    start()
