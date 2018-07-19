e580383846cfd6438f549544950055bf5e2d3c72
QiM <hokagequan@me.com>
2018-07-19 16:54:07 +0800

    主页面框架

--- ui/app.py
+++ ui/app.py
@@ -8,6 +8,14 @@ class Size(object):
 		self.width = width
 		self.height = height
 
+class Point(object):
+	"""docstring for Point"""
+	def __init__(self, x, y):
+		super(__class__, self).__init__()
+		self.x = x
+		self.y = y
+		
+
 
 window_size = Size(1200, 700)
 phone_size = Size(320, 600)
@@ -17,19 +25,31 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
+        self.init_window()
         self.create_widgets()
 
-    def create_widgets(self):
-        margin_left = 20
+    def init_window(self):
+        self.master.wm_attributes("-transparent", True)
+        self.master.config(bg="systemTransparent")
 
-        phone_window = tk.Frame(self)
+    def create_widgets(self):
+        phone_window_size = Size(window_size.width / 2, window_size.height)
+        left_top_x = (phone_window_size.width - phone_size.width) / 2
+        left_top_y = (phone_window_size.height - phone_size.height) / 2
+        left_top = Point(left_top_x, left_top_y)
+
+        # Phone container
+        phone_window = tk.Frame(self, bg="red", width=window_size.width / 2, height=window_size.height)
         phone_window.pack(side=tk.LEFT)
-        canvas = tk.Canvas(phone_window, width=window_size.width / 2, height=window_size.height)
+
+        # Phone position
+        canvas = tk.Canvas(phone_window, width=window_size.width / 2, height=window_size.height, bg="systemTransparent")
         canvas.place(x=0, y=0)
-        canvas.create_rectangle(margin_left, (window_size.height - phone_size.height) / 2, margin_left + phone_size.width, (window_size.height - phone_size.height) / 2 + phone_size.height, dash=(4, 2))
+        canvas.create_rectangle(left_top.x, left_top.y, left_top.x + phone_size.width, left_top.y + phone_size.height, dash=(10, 6), outline="green", width=3)
         canvas.pack()
         
-        op_frame = tk.Frame(self)
+        # Operation panel
+        op_frame = tk.Frame(self, width=window_size.width / 2, height=window_size.height)
         op_frame.pack(side=tk.LEFT)
 
         # self.hi_there = tk.Button(self)
