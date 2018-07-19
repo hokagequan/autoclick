84cdcab53ed628743b06df9f92186e99e94e51a7
Quan <hokagequan@me.com>
2018-07-19 21:36:18 +0800

    时间调整

--- jukandian/jukandian.py
+++ jukandian/jukandian.py
@@ -5,18 +5,19 @@ sys.path.append("..")
 
 from core import drive
 
-click_point = (270, 240)
+click_point = (170, 180)
 # back_point = (40, 1050)
-back_point = (50, 120)
-refresh_point = (60, 1000)
+back_point = (30, 100)
+refresh_point = (35, 670)
+move_distance = (5, 10)
 
 def scroll(times, distance, direction):
 	for x in range(times):
-		time.sleep(1)
+		time.sleep(10)
 		drive.scroll_mouse(0, distance * direction)
 
 def fake_read():
-	for x in range(8):
+	for x in range(1):
 		p = list(move_distance)
 		p.append(-1 if x % 2 == 0 else 1)
 		scroll(*p)
@@ -24,28 +25,28 @@ def fake_read():
 
 def work():
 	while not drive.esc_pressed:
-		time.sleep(2)
+		time.sleep(4)
 
 		# drive.get_position()
 
 		drive.position_mouse(*click_point)
-		time.sleep(1)
+		time.sleep(2)
 		drive.click()
 
-		time.sleep(5)
+		fake_read()
+
+		time.sleep(8)
 		
 		drive.position_mouse(*back_point)
-		time.sleep(1)
+		time.sleep(2)
 		drive.click()
-		# time.sleep(1)
-		# drive.click()
 
-		time.sleep(1)
+		time.sleep(2)
 		drive.position_mouse(*refresh_point)
-		time.sleep(1)
+		time.sleep(2)
 		drive.click()
 
-		time.sleep(6)
+		time.sleep(8)
 
 
 def start():
